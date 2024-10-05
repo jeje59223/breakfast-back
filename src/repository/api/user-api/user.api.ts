cca9b351f7c9e05ef906c92dadf11d19cf6eb0af
Jerome <jerome.cnockaert@leroymerlin.fr>
2024-10-05 15:03:37 +0200

    feat(breakfast-back): refacto

--- src/repository/api/user-api/user.api.ts
+++ src/repository/api/user-api/user.api.ts
@@ -6,23 +6,9 @@ export async function getUsers(): Promise<User[]> {
     try {
         const database = client.db('TEST')
         const users = database.collection('Users')
-        const allUser = await users.find({}).toArray();
+        const allUsers = await users.find<User>({}).toArray();
 
-        const mappedUsers: User[] = allUser.map(user => ({
-            _id: user._id,
-            lastname: user.lastname,
-            firstname: user.firstname,
-            email: user.email,
-            login: user.login,
-            picture: user.picture,
-            roles: user.roles,
-            numberOfBreakFastOrganised: user.numberOfBreakFastOrganised,
-            nextOrganizedBreakfastDate: user.nextOrganizedBreakfastDate,
-            creationDate: user.creationDate,
-            ldap: user.ldap
-        }));
-
-        return mappedUsers
+        return allUsers;
     } catch (err) {
         console.log(err)
         return [];
@@ -34,27 +20,9 @@ export async function getUserById(id: string): Promise<User | null> {
         const database = client.db('TEST')
         const users = database.collection('Users')
         const query = { _id: new ObjectId(id) }
-        const user = await users.findOne(query);
+        const user = await users.findOne<User>(query);  // Typage direct ici
 
-        if (!user) {
-            return null;
-        }
-
-        const mappedUser: User = {
-            _id: user._id,
-            lastname: user.lastname,
-            firstname: user.firstname,
-            email: user.email,
-            login: user.login,
-            picture: user.picture,
-            roles: user.roles,
-            numberOfBreakFastOrganised: user.numberOfBreakFastOrganised,
-            nextOrganizedBreakfastDate: user.nextOrganizedBreakfastDate,
-            creationDate: user.creationDate,
-            ldap: user.ldap
-        };
-
-        return mappedUser
+        return user || null;  // Retourne directement l'utilisateur ou null s'il n'existe pas
     } catch (err) {
         console.log(err)
         return null;
